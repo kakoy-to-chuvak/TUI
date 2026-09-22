ebd5790628e0a61b1710a4d202d1c4ddd3843d34
kakoy-to-chuvak <>
2026-09-22 21:57:04 +0300

    fix

---
+++ include/TUI_Render.h
@@ -0,0 +1,60 @@
+#ifndef __TUI_Render_h__
+#define __TUI_Render_h__
+#pragma once
+
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+
+
+#include "TUI_Symbols.h"
+#include "TUI_Error.h"
+
+
+
+typedef struct TUI_Renderer {
+        
+        FILE *stream;
+        TUI_ColorFormat color_format;
+        int color_size; // size of color of one symbol
+
+        uint32_t width;
+        uint32_t height;
+        
+        TUI_Symbol *symbols;
+        void *colors;
+        uint32_t symbols_count;
+
+        TUI_UniSColor draw_color;
+        int draw_symbol;
+
+} TUI_Renderer;
+
+
+
+TUI_Renderer *TUI_RendererCreate(FILE *_Stream, uint32_t _Width, uint32_t _Height, TUI_ColorFormat _Format);
+
+
+
+bool TUI_RendererPresent(TUI_Renderer *_Renderer);
+
+
+
+bool TUI_RendererClear(TUI_Renderer *_Renderer);
+
+
+bool TUI_RendererResize(TUI_Renderer *_Renderer, uint32_t _Width, uint32_t _Height);
+
+
+
+bool TUI_RenderSymbol(TUI_Renderer *_Renderer, uint32_t _X, uint32_t _Y, TUI_Symbol _Symbol, TUI_UniSColor _Color);
+
+
+
+
+#endif // __TUI_Render_h__
+
